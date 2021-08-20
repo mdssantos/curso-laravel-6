494b8adea98447a674905b56546ccd4c63766ccd
unknown <matheusmep@gmail.com>
2021-08-20 00:24:24 -0400

    Produto Controller

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,68 @@
 <?php
 
+
+
+Route::resource('products', 'ProductController');
+/*
+
+Route::delete('/products/{id}', 'ProductController@destroy');
+Route::put('/products/{id}', 'ProductController@update');
+Route::get('/products/{id}/edit', 'ProductController@edit');
+Route::get('/products/create', 'ProductController@create');
+Route::get('/products/{id}', 'ProductController@show');
+Route::get('/products', 'ProductController@index');
+Route::post('/products', 'ProductController@store');
+
+*/
+
 Route::get('/login', function () {
     return ('Login');
 })->name('login');
 
+
+/*
 Route::middleware([])->group(function () {
 
     Route::prefix('admin')->group(function () {
 
-        Route::get('/dashboard', function () {
-            return ('Home admin');
+        Route::namespace('Admin')-> group(function () {
+
+        Route::get('/dashboard', 'TesteController@teste')->name('admin.dashboard');
+
+        Route::get('/financeiro', 'TesteController@teste')->name('admin.financeiro');
+
+        Route::get('/produtos', 'TesteController@teste')->name('admin.produtos');
+
+        Route::get('/', function () {
+            return redirect()->route('admin.dashboard');
         });
 
-        Route::get('/financeiro', function () {
-            return ('Financeiro admin');
         });
-
-        Route::get('/produtos', function () {
-            return ('Produtos admin');
-        });
-
-        Route::get('/', 'Admin\TesteController@teste');
 
     });
 
-});
+}); */
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'name' => 'admin.'
+
+], function () {
+
+
+        Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+
+        Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+
+        Route::get('/produtos', 'TesteController@teste')->name('produtos');
+
+        Route::get('/', function () {
+            return redirect()->route('dashboard');
+        });
+
+    });
 
 
 Route::get('redirect3', function () {
